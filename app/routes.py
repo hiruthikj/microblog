af834c800320886fe07086ecef8cf5d9bea488d3
hiruthikj <hiruthik27@gmail.com>
2021-03-09 17:35:20 +0530

    feat: login form

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from . import app
 from .forms import LoginForm
@@ -8,21 +8,19 @@ from .forms import LoginForm
 @app.route("/index")
 def index():
     user = {
-        'username': '007',
+        "username": "007",
     }
     posts = [
-        {
-            'author': {'username':'kenny'},
-            'body': 'Woohoo!!!',
-        },
-        {
-            'author': {'username':'kyle'},
-            'body': 'Dude, thats nice',
-        },
+        {"author": {"username": "kenny"}, "body": "Woohoo!!!",},
+        {"author": {"username": "kyle"}, "body": "Dude, thats nice",},
     ]
-    return render_template('index.html', title='home', user=user, posts=posts)
+    return render_template("index.html", title="home", user=user, posts=posts)
 
-@app.route('/login')
+
+@app.route("/login", methods=['GET','POST'])
 def login():
-    form = LoginForm()       # pass instance not class
-    return render_template('login.html', title='login', form=form)
+    form = LoginForm()  # pass instance not class
+    if form.validate_on_submit():
+        flash(f'Login Requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template("login.html", title="login", form=form)
