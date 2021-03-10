a7c17b938f8a306a30c0ab52630834e75ec540e8
hiruthikj <hiruthik27@gmail.com>
2021-03-10 20:32:26 +0530

    feat: Add User, Post models

--- microblog.py
+++ microblog.py
@@ -2,4 +2,15 @@
 Defining the flask application instance
 """
 
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post,
+    }
+
